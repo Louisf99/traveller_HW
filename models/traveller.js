b7a73e5eda7d1a13e0753c754275e0f2cb8b05ee
Louis Fletcher <Louisco1999@gmail.com>
2021-11-24 22:51:05 +0000

    all tests pass MVP complete along with extensions

--- models/traveller.js
+++ models/traveller.js
@@ -1,39 +1,35 @@
 const Traveller = function(journeys) {
   this.journeys = journeys;
 };
-
-Traveller.prototype.getJourneyStartLocations = function() {
-  return this.journeys.map((journey) => {
-    return journey.startLocation;
-  });
-};
-
-Traveller.prototype.getJourneyEndLocations = function () {
-  return this.journeys.map((journey) => {
-    return journey.endLocation;
-  })
-};
-
-Traveller.prototype.getJourneysByTransport = function (transport) {
-  return this.journeys.filter((journey) => {
-    return journey.transport === transport;
-  });
-};
-Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-  return this.journeys.filter((journey) => {
-    return journey.distance > minDistance;
-  });
-};
-
-Traveller.prototype.calculateTotalDistanceTravelled = function () {
-  return this.journeys.reduce((total, journey) => {
-    return total + journey.distance;
-  }, 0);
-};
-
-Traveller.prototype.getUniqueModesOfTransport = function () {
-
-};
-
-
+  Traveller.prototype.getJourneyStartLocations = function() {
+    const result = this.journeys.map(a => a.startLocation);
+    return result;
+  };
+  
+  Traveller.prototype.getJourneyEndLocations = function () {
+    const result = this.journeys.map(a => a.endLocation);
+    return result;
+  };
+  
+  Traveller.prototype.getJourneysByTransport = function (transport) {
+    let result = this.journeys.filter(a => a.transport == transport)
+    return result;
+  };
+  
+  Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+    let result = this.journeys.filter(a => a.distance > minDistance);
+    return result;
+  };
+  
+  Traveller.prototype.calculateTotalDistanceTravelled = function () {
+    let result = this.journeys.reduce((previousValue, currentValue) => previousValue + currentValue.distance, 0);
+    return result;
+  };
+  
+  Traveller.prototype.getUniqueModesOfTransport = function () {
+    const transports = this.journeys.map(a => a.transport);
+    const result = transports.filter((item, index) => transports.indexOf(item) === index);
+    return result;
+  };
+  
 module.exports = Traveller;
